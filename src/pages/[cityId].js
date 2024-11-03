0e0350b5127afde22ea7e3cb04d6d3d374d1113b
LazarencoAna <lvncax@gmail.com>
2024-11-03 18:31:26 +0200

    Add pages

---
+++ src/pages/[cityId].js
@@ -0,0 +1,7 @@
+export default function CityPage() {
+  return (
+    <div>
+      <p> This is city page</p>
+    </div>
+  );
+}
---
+++ src/pages/search.js
@@ -0,0 +1,7 @@
+export default function Search() {
+  return (
+    <div>
+      <p> This is search page</p>
+    </div>
+  );
+}
